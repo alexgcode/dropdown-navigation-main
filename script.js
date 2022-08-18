f51d1ec9c4369736bed02cf279f110edcae9d7bf
ALEXGF <alexgf2703@gmail.com>
2022-08-18 17:53:37 -0500

    ordering code

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const closeMenu = document.querySelector(".close-menu");
 const navbarMenu = document.querySelector(".navbar");
 
 toggle.addEventListener("click", () => {
-    navbarMenu.classList.add("navbar-menu-active");
+    navbarMenu.classList.add("navbar-active");
 });
 
 closeMenu.addEventListener("click", () => {
-    navbarMenu.classList.remove("navbar-menu-active");
+    navbarMenu.classList.remove("navbar-active");
 })
 
 
 //dropdown section
-
 document.querySelectorAll(".dropdown-container").forEach(function(dropdown){
     dropdown.addEventListener("click", function(){
         dropdown.lastElementChild.classList.toggle("show");
