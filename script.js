4d2d20563822da87596b84a67ff67ec8911e1807
ALEXGF <alexgf2703@gmail.com>
2022-08-19 18:56:39 -0500

    adding dark background to mobile navbar and fixing dropdown position

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 const toggle = document.querySelector(".toggle");
 const closeMenu = document.querySelector(".close-menu");
 const navbarMenu = document.querySelector(".navbar");
+const backgroundMobile = document.querySelector(".background-menu-mobile");
 
 toggle.addEventListener("click", () => {
     navbarMenu.classList.add("navbar-active");
+    backgroundMobile.classList.add("navbar-active");
 });
 
 closeMenu.addEventListener("click", () => {
     navbarMenu.classList.remove("navbar-active");
+    backgroundMobile.classList.remove("navbar-active");
 })
 
 
